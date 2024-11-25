5ff4db7c27519eb78e9887c890bee8be6cd0f1d0
Ronjohnson Owuor <ronjohnsonowuor83@gmail.com>
2024-11-25 20:06:08 +0300

    pausing this project I just feel I don't have the motivation to continue

--- modules/profilecheck.ts
+++ modules/profilecheck.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { user_availlable } from "../helpers/token_valid";
 import data_source from "../datasource/datasource";
 import { User } from "../entities/User.entity";
+import moment from "moment";
+import { Userdetails } from "../entities/Userdetails.entity";
 
 export const profileCheck = async (req: Request, res: Response) => {
   const bearer_token = req.headers.authorization;
@@ -15,8 +17,26 @@ export const profileCheck = async (req: Request, res: Response) => {
         .set({ profile_url: profile_image! })
         .where("id = :id", { id: user?.id })
         .execute();
+        const nick_name = user?.username.trim().replace(' ','_');
+        const user_id = user?.id;
+        const tier = 0;  //0 -> free tier, 1 -> monthly 2 -> yearly;
+        const expiry_date = moment(user?.createdAt).add(7,'days').toISOString();
+        const createdAt = moment().toISOString();
+        const updatedAt = createdAt;
+        await data_source.createQueryBuilder().insert().into(Userdetails).values([
+          {user_id:user_id,nickname:nick_name,tier,expiry_date,createdAt,updatedAt}
+        ]).execute();
     }
-    return res.json({ proceed,user });
+    const user_data = await data_source
+    .getRepository(Userdetails)
+    .createQueryBuilder("userdetails").select([
+      "userdetails.nickname",
+      "userdetails.tier",
+      "userdetails.expiry_date"
+    ])
+    .where("userdetails.user_id = :user_id", { user_id: user?.id })
+    .getOne();
+    return res.json({ proceed,user,user_data });
   }
-  return res.json({ proceed,user });
+  return res.json({ proceed,user,user_data:null});
 };
